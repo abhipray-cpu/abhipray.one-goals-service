08c5591a54b35a6677ce81289d21a0c275f63de7
abhipray-cpu <dumkaabhipray@gmail.com>
2024-07-21 19:10:19 +0530

    goals-service

---
+++ src/test/java/one/abhipray/Goals_Service/GoalsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package one.abhipray.Goals_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
